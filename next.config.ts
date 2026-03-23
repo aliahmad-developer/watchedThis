3a2b3d436dc2b08a0b508ada56a59fd33fc7a4a7
Missy <aliahmad.developer@gmail.com>
2026-03-23 19:15:44 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,26 @@
 const nextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 60 * 60 * 24 * 7,
+    formats: ['image/webp'],                    // drop avif
+    minimumCacheTTL: 60 * 60 * 24 * 31,        // 31 days
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [64, 128, 256, 500],
-    qualities: [55, 75],
+    qualities: [75,55],                            // simplify unless you use 55 explicitly
     remotePatterns: [
       { protocol: "https", hostname: "image.tmdb.org", pathname: "/**" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "res.cloudinary.com" },
     ],
   },
- experimental: {
-  optimizePackageImports: [
-    'lucide-react',
-    '@fortawesome/react-fontawesome',
-    '@fortawesome/free-solid-svg-icons',
-    '@fortawesome/free-brands-svg-icons',
-    'framer-motion',
-    '@heroicons/react',
-  ],
-},
+  experimental: {
+    optimizePackageImports: [
+      'lucide-react',
+      '@fortawesome/react-fontawesome',
+      '@fortawesome/free-solid-svg-icons',
+      '@fortawesome/free-brands-svg-icons',
+      'framer-motion',
+      '@heroicons/react',
+    ],
+  },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
