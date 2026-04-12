292e1d1aef38218a9020f4fc443bc317d8cb31a1
Missy <aliahmad.developer@gmail.com>
2026-04-12 16:08:56 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,7 @@ export const metadata: Metadata = {
     follow: true,
   },
   icons: {
+    shortcut: "/favicon.ico", 
     icon: [
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
