5b93ddba65c1411689749b4ff97dc9d0e080475f
Missy <aliahmad.developer@gmail.com>
2026-05-21 12:14:03 +0500

    Update page.tsx

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,14 @@
+// app/auth/page.tsx
+import { Suspense } from "react";
 import AuthPage from "../components/auth/authPage";
+
 export default function Page() {
   return (
     <>
       <h1 className="sr-only">Authentication | WatchedThis</h1>
-      <AuthPage />
+      <Suspense>
+        <AuthPage />
+      </Suspense>
     </>
   );
 }
